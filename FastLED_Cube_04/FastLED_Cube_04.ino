cd849d798064c7a1995ee10e7e8161c2e32fe943
Sam Weiss <sammytom@gmail.com>
2022-12-28 17:52:37 -0800

    Update FastLED_Cube_04.ino
    Added collision detection. Arduino NANO doesn't have enough RAM.

--- FastLED_Cube_04/FastLED_Cube_04.ino
+++ FastLED_Cube_04/FastLED_Cube_04.ino
@@ -2,39 +2,53 @@
 #include "FastLED.h"
 #include "Cube.h"
 
-Cube Cube; // Initilize Cube lib
-
-MPU6050 accelgyro; // Initilize IMU
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
 #define LED_PIN     13
 #define NUM_LEDS    384
 #define BRIGHTNESS  20
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS]; // Initilize LED arrays
 
-int voxel[3] = {9,4,4};
+CRGB leds[NUM_LEDS]; // Initilize LED arrays
+Cube Cube; // Initilize Cube lib
+MPU6050 accelgyro; // Initilize IMU
+
+int voxel[3] = {4,4,9};
 int dVal = 100;
+
+// void Maze() {
+//   int wall[8][3] = {{1,1,9},{2,1,9},{3,1,9},{4,1,9},{5,1,9},{6,1,9},{7,1,9},{8,1,9}};
+//   for (byte i = 0; i <= 7; i++) {
+//     int pix = Cube.deVoxel(wall[i]);
+//     leds[pix] = CRGB::Green;
+//   }
+//   FastLED.show();
+//   return(0);
+// };
 
 void Move(int ord1, int ord2, int dist1, int dist2) {
   int pix = Cube.deVoxel(voxel);
-  leds[pix] = CRGB::Black;
   voxel[ord1] = voxel[ord1] + dist1;
   voxel[ord2] = voxel[ord2] + dist2;
-  pix = Cube.deVoxel(voxel);
-  leds[pix] = CRGB::White;
-  FastLED.show();
-  delay(dVal);
+  int new_pix = Cube.deVoxel(voxel);
+  if (leds[new_pix].getAverageLight() != 0) {
+    voxel[ord1] = voxel[ord1] - dist1;
+    voxel[ord2] = voxel[ord2] - dist2;
+  }
+  else {
+    leds[pix] = CRGB::Black;
+    leds[new_pix] = CRGB::White;
+    FastLED.show();
+    delay(dVal);
+  }
+  return(0);
 }
 
 void setup() {
   delay(2000); // power-up safety delay
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   set_max_power_in_volts_and_milliamps(5,5600);
+  // Maze();
   Wire.begin();
   accelgyro.initialize();
   int pix = Cube.deVoxel(voxel);
@@ -44,14 +58,13 @@ void setup() {
 }
 
 void loop() {
-  int aVal = 10000;
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  int accels[3];
-  accels[0] = ax;
-  accels[1] = ay;
-  accels[2] = az;
-  for (int i = 0; i <= 2; i++) {
-    for (int j = 0; j <=2; j++) {
+  int aVal = 8000;
+  int16_t accels[3];
+  accels[0] = accelgyro.getAccelerationX();
+  accels[1] = accelgyro.getAccelerationY();
+  accels[2] = accelgyro.getAccelerationZ();
+  for (byte i = 0; i <= 2; i++) {
+    for (byte j = 0; j <=2; j++) {
       if ((i != j) && (accels[i] > aVal) && (voxel[i] > 1) && ((voxel[j] == 0) || (voxel[j] == 9))) {
         Move(i,0,-1,0);
       }
